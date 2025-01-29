40eefd00ef38ba662b0ff9577507e84c20268da0
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2025-01-29 11:29:06 +0000

    MAINT: refactor the main.py module to improve readability
    Make main.py more readable

--- src/main.py
+++ src/main.py
@@ -11,30 +11,32 @@ def main() -> bool:
     The entry point function for the action.
     Here we get environment variables then set environment variables when finished.
     """
+    # Check if the action should skip version checks
+    for label in os.environ.get("INPUT_LABELS"):
+        if label in ["documentation", "workflow"]:
+            return False
+
+    # Collect various paths from the environment
     app_path = Path(os.environ.get("INPUT_APP_VERSION_PATH"))
-    compose_path = os.environ.get("INPUT_DOCKER_COMPOSE_PATH")
+    compose_path = Path(os.environ.get("INPUT_DOCKER_COMPOSE_PATH"))
     root_path = Path(os.environ.get("GITHUB_WORKSPACE"))
     main_path = root_path / "main"
     branch_path = root_path / "branch"
-    with open(branch_path / app_path, "r", encoding="utf-8") as release_file:
-        release_version = release_file.read().strip("\n")
 
-    labels = os.environ.get("INPUT_LABELS")
-    if any(label in labels for label in ["documentation", "workflow"]):
-        return False
-
+    # Action must compare the app version as the minimum feature.
     CompareAppVersion().run(main_path / app_path, branch_path / app_path)
+
+    # Compare the Docker compose file version if given
     if compose_path:
-        compose_path = Path(compose_path)
         CompareComposeVersion().run(branch_path / app_path, branch_path / compose_path)
 
-    github_env = os.getenv("GITHUB_ENV")
-    with open(github_env, "a", encoding="utf-8") as env:
-        # We can assume either/both of these values returned true otherwise they would have errored
+    with open(os.getenv("GITHUB_ENV"), "a", encoding="utf-8") as env:
+        # We can assume these values returned true otherwise they would have raised an error.
         env.write("app_updated=true\n")
+        # If Docker compose path was provided we can assume it returned true otherwise there would be an error.
         if compose_path:
             env.write("compose_updated=true")
-        env.write(f"release_tag={release_version}")
+
     return True
 
 
